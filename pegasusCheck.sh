2323d918825fbecee7e98c1f2baed9cdfd353d48
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2021-11-05 19:54:37 -0600

    Update pegasusCheck.sh

--- pegasusCheck.sh
+++ pegasusCheck.sh
@@ -35,10 +35,6 @@ echo -e "${purpleColour}"'[*] Installing requirements.'"${endColour}"
 echo -e ' '
 echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
 
-#if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-#    echo -e -n "${redColour}"'Not running as root \nExiting...'"${endColour}"
-#    exit
-#fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]
 then
@@ -51,8 +47,8 @@ echo -e -n "${greenColour}"' Operating system not supported, exitting... '"${end
 exit 0
 fi
 
-#if [ $so = "linux" ]
-#then
+if [ $so = "linux" ]
+then
 
 echo -e -n "${greenColour}"'Installing misc...'"${endColour}"
 echo -e ' '
@@ -136,7 +132,6 @@ fi
 echo -e ' '
 
 echo -e "${greenColour}"'Downloading the pegasus IOCs...'"${endColour}"
-cd - > /dev/null 2>&1
 wget https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/pegasus.stix2 > /dev/null 2>&1
 sleep 0.5
 echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
@@ -221,3 +216,168 @@ fi
 rm -rf results.txt
 echo -e ' '
 echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+
+echo -e ' '
+#Running on MacOS
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+elif [ $so = "mac" ]
+then
+echo -e -n "${greenColour}"'Checking for brew...'"${endColour}"
+echo -e ' '
+sleep 0.5
+if ! command -v brew &> /dev/null
+then
+    echo -e "${redColour}"'Brew not installed, exiting...'"${endColour}"
+    exit 0
+else
+    echo -e "${greenColour}"'Brew installed, skipping...'"${endColour}"
+fi
+echo -e ' '
+
+echo -e -n "${greenColour}"'Installing misc...'"${endColour}"
+echo -e ' '
+yes | brew install wget git libusb3 > /dev/null 2>&1
+sleep 0.5
+echo -e ' '
+
+echo -e -n "${greenColour}"'Checking for idevicebackup2...'"${endColour}"
+echo -e ' '
+sleep 0.5
+
+if ! command -v idevicebackup2 &> /dev/null
+then
+    echo -e -n "${greenColour}"'Installing' "${blueColour}"'idevicebackup2...'"${endColour}""${endColour}"
+    echo -e ' '
+    #install usbmuxd libimobiledevice6 libimobiledevice-utils ideviceinstaller -y > /dev/null 2>&1
+    yes | brew install libimobiledevice > /dev/null 2>&1
+    yes | brew install ideviceinstaller > /dev/null 2>&1
+    echo -e ' '
+    source ~/.profile
+    sleep 0.5
+else
+echo -e "${redColour}"'idevicebackup2 exists, skipping...'"${endColour}"
+fi
+echo -e ' '
+
+echo -e -n "${greenColour}"'Checking for python3...'"${endColour}"
+echo -e ' '
+sleep 0.5  
+
+if ! command -v python3 &> /dev/null
+then
+    echo -e -n "${greenColour}"'Installing' "${blueColour}"'python3...'"${endColour}""${endColour}"
+    echo -e ' '
+    #sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev > /dev/null 2>&1
+    yes | brew install python3 > /dev/null 2>&1
+    echo -e ' '
+    source ~/.profile
+    sleep 0.5
+else
+echo -e "${redColour}"'python3 exists, skipping...'"${endColour}"
+fi
+echo -e ' '
+
+echo -e "${greenColour}"'Checking for mvt-ios...'"${endColour}"
+sleep 0.5
+
+if ! command -v mvt-ios &> /dev/null
+then
+    echo -e -n "${greenColour}"'Installing' "${blueColour}"'mvt-ios...'"${endColour}""${endColour}"
+    echo -e ' '
+    pip3 install mvt > /dev/null 2>&1
+    echo -e ' '
+    sleep 0.5
+
+else
+echo -e "${redColour}"'mvt-ios exists, skipping...'"${endColour}"
+fi
+
+echo -e ' '
+
+echo -e "${greenColour}"'Downloading the pegasus IOCs...'"${endColour}"
+wget https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/pegasus.stix2 > /dev/null 2>&1
+sleep 0.5
+echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+
+echo -e ' '
+
+echo -e "${purpleColour}"'[*] Backup the iPhone. '"${endColour}"
+echo -e ' '
+echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+echo -e -n "${greenColour}"'Connect the iPhone to the PC now...'"${endColour}"
+echo -e ' '
+
+echo -e "${greenColour}"'You must select '"${endColour}" "${turquoiseColour}"'-Trust this computer-'"${endColour}" "${greenColour}"' on the iPhone'"${endColour}"
+sleep 0.5
+
+echo -e ' '
+
+echo -e "${yellowColour}"'Waiting 15 seconds...'"${endColour}"
+echo -e ' '
+
+echo -e "${greenColour}"'Preparing the backup...'"${endColour}"
+sleep 0.5
+echo -e ' '
+
+echo -e -n "${greenColour}"'Name of the backup directory: '"${endColour}"
+read dirName
+mkdir $dirName
+sleep 0.5
+echo -e ' '
+
+unset password
+prompt=$(echo -e -n "${yellowColour}"'Password to encrypt the iPhone Backup: '"${endColour}")
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    if [[ $char == $'\0' ]]
+    then
+        break
+    fi
+    prompt='*'
+    password+="$char"
+done
+echo -e '\n'
+
+sleep 0.5
+echo -e "${grayColour}"'Enabling the backup encryption '"${endColour}"
+echo -e ' '
+idevicebackup2 backup -i encryption on $password $dirName
+encryptedBackup=$(ls $dirName)
+
+sleep 0.5
+echo -e ' '
+echo -e "${grayColour}"'Making the backup (this may take a while...)'"${endColour}"
+echo -e ' '
+idevicebackup2 backup --full $dirName > /dev/null 2>&1
+
+sleep 0.5
+echo -e "${grayColour}"'Decrypting the backup '"${endColour}"
+mvt-ios decrypt-backup -p $password -d Decrypted $dirName/$encryptedBackup > /dev/null 2>&1
+echo -e ' '
+
+sleep 0.5
+echo -e "${grayColour}"'Extracting the results...'"${endColour}"
+mvt-ios check-backup --output resultsPegasus Decrypted --iocs pegasus.stix2 | tee results.txt
+echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+echo -e ' '
+echo -e "${purpleColour}"'[*] Final results: '"${endColour}"
+
+cat results.txt | grep WARNING > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+echo -e ' '
+echo -e "${yellowColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+echo -e "${redColour}"'[*] Malware encountered!!'"${endColour}"
+echo -e "${yellowColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+echo -e ' '
+cat results.txt | grep WARNING
+echo -e ' '
+echo -e "${redColour}"'Check the indicators above, as well as the .json files with'"${endColour}" "${purpleColour}"'_detected'"${endColour}" "${redColour}"'ending in the '"${endColour}""${yellowColour}"'/resultsPegasus '"${endColour}""${redColour}"'folder.'"${endColour}"
+else
+echo -e "${turquoiseColour}"'No indicators that you have been infected by pegasus are found.'"${endColour}"
+fi
+rm -rf results.txt
+echo -e ' '
+echo -e "${blueColour}"'--------------------------------------------------------------------------------------------------------'"${endColour}"
+fi
+echo -e ' '
